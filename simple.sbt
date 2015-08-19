71eec9f7c7888fafdefada2e2bd1b5596f2e9333
Elf Qi <qihe@tju.edu.cn>
2015-08-20 04:49:40 +1000

    load whole database

--- simple.sbt
+++ simple.sbt
@@ -3,14 +3,12 @@ name := "Data Loader"
 
 
 lazy val commonSettings = Seq(
-	scalaVersion := "2.11.6",
+	scalaVersion := "2.10.4",
 	version := "1.0",
 	updateOptions := updateOptions.value.withCachedResolution(true),
 	libraryDependencies ++= Seq(
     "org.apache.spark" %% "spark-core" % "1.4.0",
     "org.apache.spark" %% "spark-sql" % "1.4.0",
-    "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
-    "ch.qos.logback" % "logback-classic" % "1.1.3",
     "org.postgresql" % "postgresql" % "9.3-1103-jdbc41"
    )
 )
--- src/main/scala/DataLoader.scala
+++ src/main/scala/DataLoader.scala
@@ -2,38 +2,48 @@ package com.servian.spark
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.functions._
-import com.typesafe.scalalogging._
-import org.slf4j.LoggerFactory
 
 object DataLoader {
+	val DRIVER = "org.postgresql.Driver"
+	val URL = "jdbc:postgresql://localhost/campaign?user=ds2&password=hadoop123"
 
 	def main(args:Array[String]) = {
-		val logger = Logger(LoggerFactory.getLogger(this.getClass))
 
 		val conf = new SparkConf().setAppName("Data Loader")
 
 		val sc = new SparkContext(conf)
 		val sqlContext = new SQLContext(sc)
+		//val options = scala.collection.mutable.Map(
+		//		"driver" 	-> DRIVER,
+		//		"url" 		-> URL,
+				//"dbtable"	-> "customer",
+				//"partitionColumn" -> "customer_id",
+				//"lowerBound" -> "1",
+				//"upperBound" -> "1000000",
+				//"numPartitions" -> "1"
+		//	)
 
-
-		val options = Map(
-				"driver" 	-> "org.postgresql.Driver",
-				"url" 		-> "jdbc:postgresql://localhost/ds2?user=ds2&password=hadoop123",
-				"dbtable"	-> "customers",
-				"partitionColumn" -> "customerid",
-				"lowerBound" -> "1",
-				"upperBound" -> "20",
-				"numPartitions" -> "1"
+		val tables = Map("\"CUSTOMER\"" -> "\"CUSTOMER_ID\"",
+						 "\"PRODUCT\"" -> "\"PRODUCT_ID\"",
+						 "\"CAMPAIGN\"" -> "\"CAMPAIGN_ID\"",
+						 "\"CONTACT_HISTORY\"" -> "\"HISTORY_ID\"",
+						 "\"RESPONSE\"" -> "\"RESPONSE_ID\"",
+						 "\"SERVICE\"" -> "\"SERVICE_ID\""
 			)
-
-		logger.warn("before load")
-		val jdbcDataFrame = sqlContext.load("jdbc", options)
-		jdbcDataFrame.registerTempTable("customers")
-		jdbcDataFrame.write.parquet("hdfs:///user/hadoop/parquet_files/customers")
-		val results = sqlContext.sql("SELECT count(firstname) FROM customers WHERE customerid < 10 order by customerid")
-		results.map(t => "firstname: " + t(0) + " lastname: " + t(1)).collect().foreach(println)
-
-		logger.warn("after load")
+		tables.foreach{(table: (String, String)) => 
+				val options = Map(
+					"driver" -> DRIVER,
+					"url" -> URL,
+					"dbtable" -> table._1
+					//"partitionColumn" -> table._2,
+				)
+				
+				val jdbcDataFrame = sqlContext.read.format("jdbc").options(options).load()
+				//jdbcDataFrame.registerTempTable(table._1)
+				jdbcDataFrame.write.parquet("hdfs:///user/hadoop/parquet_files/" + table._1)
+				//val results = sqlContext.sql("SELECT count(firstname) FROM customers WHERE customerid < 10 order by customerid")
+				//results.map(t => "firstname: " + t(0) + " lastname: " + t(1)).collect().foreach(println)
+			}
 
 		sc.stop() 
 	}
